a8fc06d6524cb56b98acfc8759ec135a42f5f36b
Xephy <adr3nalinexin@gmail.com>
2018-09-14 19:48:41 +0200

    Almost done with header

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssVars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+    .on('error', (err) => {
+      console.log(err.toString());
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
