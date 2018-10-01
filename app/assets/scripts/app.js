ca0a40226a5ee978a0165616c1883aaa03f1ed6f
Xephy <adr3nalinexin@gmail.com>
2018-10-01 14:43:50 +0200

    added AOS library

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,13 @@
-const stickyNavbar = require('./modules/sticky');
-const AOS = require('aos');
+import stickyNavbar from './modules/sticky';
+import AOS from 'aos';
 
-AOS.init();
+AOS.init({
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false // if true, will add content of `data-aos` as classes on scroll
+});
 
 
 window.onscroll = () => {
