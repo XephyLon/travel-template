673d6693380aa6bd7f9cc432dabb0bc491bb6718
Xephy <adr3nalinexin@gmail.com>
2018-10-01 15:46:44 +0200

    removing unnecessary comments

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -5,11 +5,11 @@ import navburger from './modules/navburger';
 navburger();
 
 AOS.init({
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false // if true, will add content of `data-aos` as classes on scroll
+  disable: false,
+  startEvent: 'DOMContentLoaded',
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate',
+  useClassNames: false
 });
 
 
